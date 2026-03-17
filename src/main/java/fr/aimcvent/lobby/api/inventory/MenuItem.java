d38d1e100a3508b687b24c919649fdee6ba6ffef
Neutron-Pro <neutron@neutronstars.fr>
2026-03-17 19:10:58 +0100

    Update Icon

--- src/main/java/fr/aimcvent/lobby/api/inventory/MenuItem.java
+++ src/main/java/fr/aimcvent/lobby/api/inventory/MenuItem.java
@@ -1,9 +1,14 @@
 package fr.aimcvent.lobby.api.inventory;
 
 import fr.aimcvent.paper.api.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public interface MenuItem {
     String identifier();
+
+    default ItemStack icon(ItemStack origin) {
+        return origin;
+    }
 
     void execute(Inventory inventory, Context context);
 }
